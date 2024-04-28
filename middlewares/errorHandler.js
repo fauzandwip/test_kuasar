7d331063561adf90833360938e67396e6605bda9
fauzandwip <fauzan27dp@gmail.com>
2024-04-28 19:54:24 +0700

    feat: create type and resolver for user Login implement jsonwebtoken

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,26 +1,40 @@
 const { GraphQLError } = require('graphql');
+const { ApolloServerErrorCode } = require('@apollo/server/errors');
 
 const errorHandler = async (err) => {
 	let message, messages, code, statusCode;
 
 	switch (err.name) {
-		case 'SequelizeUniqueConstraintError':
-			code = 'BAD_USER_INPUT';
-			message = err.errors[0].message;
-			statusCode = 409;
-			break;
-
 		case 'SequelizeValidationError':
-			code = 'BAD_USER_INPUT';
+			code = ApolloServerErrorCode.BAD_USER_INPUT;
+			statusCode = 400;
 			messages = err.errors.map((error) => {
 				return error.message;
 			});
+			break;
+
+		case 'BadRequest':
+			code = ApolloServerErrorCode.BAD_USER_INPUT;
 			statusCode = 400;
+			message = err.message;
+			break;
+
+		case 'Unauthenticated':
+			code = 'UNAUTHENTICATED';
+			statusCode = 401;
+			message = err.message;
+			break;
+
+		case 'SequelizeUniqueConstraintError':
+			code = ApolloServerErrorCode.BAD_USER_INPUT;
+			statusCode = 409;
+			message = err.errors[0].message;
 			break;
 
 		default:
-			code = 'INTERNAL_SERVER_ERROR';
+			code = ApolloServerErrorCode.INTERNAL_SERVER_ERROR;
 			statusCode = 500;
+			message = 'Internal Server Error';
 			break;
 	}
 
--- schemas/user.js
+++ schemas/user.js
@@ -1,10 +1,13 @@
+const { comparePassword } = require('../helpers/bcrypt');
 const dateScalar = require('../helpers/dateScalar');
+const { signToken } = require('../helpers/jwt');
 const errorHandler = require('../middlewares/errorHandler');
 const { User } = require('../models');
 
 const userTypeDefs = `#graphql
   scalar Date
 
+  # user type
   type User {
     id: ID,
     username: String,
@@ -14,18 +17,53 @@ const userTypeDefs = `#graphql
     updatedAt: Date
   }
 
-  input NewUser {
+  # user login data
+  type UserLogin {
+    id: ID,
     username: String,
-    email: String,
-    password: String,
+    email: String
   }
 
+  # input while user register
+  input NewUser {
+    username: String!,
+    email: String!,
+    password: String!,
+  }
+
+  # Response Success Interface
+  interface ResponseSuccess {
+    code: String!
+    success: Boolean!
+    message: String!
+  }
+
+  # Response Success Register
+  type ResponseRegister implements ResponseSuccess {
+    code: String!,
+    success: Boolean!
+    message: String!,
+    user: User
+  }
+
+  # Response Success Login
+  type ResponseLogin implements ResponseSuccess {
+    code: String!,
+    success: Boolean!
+    message: String!,
+    access_token: String!
+    user: UserLogin
+  }
+
+  # query user
   type Query {
     user: User
   }
 
+  # mutation user
   type Mutation {
-    register(user: NewUser): User
+    register(user: NewUser): ResponseRegister
+    login(email: String!, password: String!): ResponseLogin
   }
 `;
 
@@ -41,9 +79,57 @@ const userResolvers = {
 					password,
 				});
 
-				return newUser;
+				return {
+					code: 201,
+					success: true,
+					message: 'Successfully Register',
+					user: newUser,
+				};
 			} catch (error) {
-				console.log(error.name);
+				throw await errorHandler(error);
+			}
+		},
+		login: async (_, args) => {
+			try {
+				const { email, password } = args;
+
+				if (!email) {
+					throw {
+						name: 'BadRequest',
+						message: 'Email is required',
+					};
+				}
+
+				if (!password) {
+					throw {
+						name: 'BadRequest',
+						message: 'Password is required',
+					};
+				}
+
+				const user = await User.findOne({ where: { email } });
+
+				if (!user || !comparePassword(password, user.password)) {
+					throw {
+						name: 'Unauthenticated',
+						message: 'Invalid email/password',
+					};
+				}
+
+				const accessToken = signToken({ id: user.id, email: user.email });
+
+				return {
+					code: 200,
+					success: true,
+					message: 'Successfully Login',
+					access_token: accessToken,
+					user: {
+						id: user.id,
+						username: user.username,
+						email: user.email,
+					},
+				};
+			} catch (error) {
 				throw await errorHandler(error);
 			}
 		},
